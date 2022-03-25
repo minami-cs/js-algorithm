3e8fb7696e7bd3e449d8590f454485a8d89e210f
Mina <mina9301@gmail.com>
2022-03-26 01:47:06 +0900

    Done: 다리를 건너는 트럭 문제 해결

--- Lv2/truck-crossing-the-bridge.js
+++ Lv2/truck-crossing-the-bridge.js
@@ -1,13 +1,27 @@
 function solution(bridge_length, weight, truck_weights) {
+  // 길이가 bridge_length가 최대인 배열에 순서대로 넣되 요소의 총합이 다리 길이를 넘어서는 안됨
+  let bridge = Array.from({ length: bridge_length }, () => 0); // 배열 생성 및 초기화 같이 할 때는 Array.from() 사용
   let result = 0;
-  while (true) {
-    // 길이 2인 배열에 순서대로 넣되 요소의 총합이 다리 길이를 넘어서는 안됨.
-    let temp = [];
-    let truck = truck_weights.shift();
-    if (truck <= weight) {
-      temp.push(truck);
+
+  // 다리를 건너는 트럭이 있는 동안
+  while (bridge.length) {
+    // queue가 하나씩 이동하도록
+    bridge.shift();
+    result++;
+
+    // 트럭이 아직 남아있을 때
+    if (truck_weights.length > 0) {
+      let sumTruck = bridge.reduce((prev, curr) => prev + curr, 0);
+      // 트럭을 넣을 수 있을 때
+      if (sumTruck + truck_weights[0] <= weight) {
+        let truck = truck_weights.shift();
+        bridge.push(truck);
+      } else {
+        bridge.push(0);
+      }
     }
   }
+  return result;
 }
 
 const bridge_length1 = 2;
@@ -23,5 +37,5 @@ const weight3 = 100;
 const truck_weights3 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
 
 console.log(solution(bridge_length1, weight1, truck_weights1));
-console.log(solution(bridge_length2, weight2, truck_weights2));
-console.log(solution(bridge_length3, weight3, truck_weights3));
+// console.log(solution(bridge_length2, weight2, truck_weights2));
+// console.log(solution(bridge_length3, weight3, truck_weights3));
