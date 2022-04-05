74a0b064ebfc187a3c4b3bd880b437318b0946c8
Mina <mina9301@gmail.com>
2022-04-05 22:21:57 +0900

    Done: 타겟 넘버 문제 해결

--- Lv2/target-number.js
+++ Lv2/target-number.js
@@ -1,4 +1,24 @@
 function solution(numbers, target) {
+  let answer = 0;
+
+  // DFS로 풀기
+  // 트리에서 왼쪽 서브트리로 가는 것은 +, 오른쪽 서브트리로 가는 것은 -
+  const dfs = (level, sum) => {
+    if (level === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+
+    // 왼쪽으로
+    dfs(level + 1, sum + numbers[level]);
+    // 오른쪽으로
+    dfs(level + 1, sum - numbers[level]);
+  };
+
+  dfs(0, 0);
+
   return answer;
 }
 
