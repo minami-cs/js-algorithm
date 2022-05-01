000578f92818e54852ea4d4ee1f57e9b51d9fb03
Mina <mina9301@gmail.com>
2022-05-01 20:53:48 +0900

    Add: 가장 먼 노드 문제 생성

---
+++ Lv3/the-farthest-node.js
@@ -0,0 +1,19 @@
+function solution(n, edge) {
+  // 최단거리 -> BFS로 풀기
+  const queue = [];
+  const visited = [];
+
+  return answer;
+}
+
+console.log(
+  solution(6, [
+    [3, 6],
+    [4, 3],
+    [3, 2],
+    [1, 3],
+    [1, 2],
+    [2, 4],
+    [5, 2],
+  ])
+);
