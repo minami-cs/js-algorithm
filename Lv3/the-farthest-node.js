5a16fd1a80a151d576df65e92ba051fad13de3dd
Mina <mina9301@gmail.com>
2022-05-03 23:05:12 +0900

    Done: 가장 먼 노드 문제 해결

--- Lv3/the-farthest-node.js
+++ Lv3/the-farthest-node.js
@@ -1,11 +1,34 @@
 function solution(n, edge) {
   // 최단거리 -> BFS로 풀기
-  let answer = 0;
-  const queue = [];
-  const visited = Array.from({ length: n + 1 }, () => false);
-
-  return answer;
+  return bfs(1, edge, n);
 }
+
+const bfs = (start, nodes, end) => {
+  const queue = [start];
+  const visited = Array.from({ length: end + 1 }, () => false);
+  const distance = Array.from({ length: end + 1 }, () => 0);
+
+  visited[start] = true;
+
+  while (queue.length) {
+    const curr = queue.shift();
+    const d = distance[curr] + 1;
+
+    for (let node of nodes) {
+      if (node[0] === curr && !visited[node[1]]) {
+        queue.push(node[1]);
+        visited[node[1]] = true;
+        distance[node[1]] = d;
+      } else if (node[1] === curr && !visited[node[0]]) {
+        queue.push(node[0]);
+        visited[node[0]] = true;
+        distance[node[0]] = d;
+      }
+    }
+  }
+  const maxDistance = Math.max.apply(null, distance);
+  return distance.filter((d) => d === maxDistance).length;
+};
 
 console.log(
   solution(6, [
