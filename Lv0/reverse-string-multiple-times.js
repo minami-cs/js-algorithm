e06a1f3a318b64a39bd289275e5bc9103a95d383
Mina <mina9301@gmail.com>
2023-06-11 22:13:47 +0900

    feat: 문자열 여러 번 뒤집기 문제 해결

---
+++ Lv0/reverse-string-multiple-times.js
@@ -0,0 +1,21 @@
+function solution(my_string, queries) {
+  queries.forEach((query) => {
+    let answer = [...my_string];
+    console.log(answer);
+    const phrase = answer.slice(query[0], query[1] + 1);
+    const reversedPhrase = phrase.reverse().join("");
+
+    answer.splice(query[0], query[1] - query[0] + 1, reversedPhrase);
+    my_string = answer.join("");
+    console.log(my_string);
+  });
+
+  // return my_string.join("");
+}
+
+solution("rermgorpsam", [
+  [2, 3],
+  [0, 7],
+  [5, 9],
+  [6, 10],
+]);
