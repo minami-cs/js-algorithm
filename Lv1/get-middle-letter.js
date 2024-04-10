48c9abf6320694ac47848fc0bd61218d77c1e029
Mina <mina9301@gmail.com>
2024-04-11 00:42:20 +0900

    feat: lv 1. 가운데 글자 가져오기 문제 해결

---
+++ Lv1/get-middle-letter.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  return s.length % 2 === 0
+    ? s.slice(s.length / 2 - 1, s.length / 2 + 1)
+    : s.slice(s.length / 2, s.length / 2 + 1);
+}
+
+console.log(solution("abcde"));
+console.log(solution("qwer"));
+console.log(solution("f"));
+console.log(solution("gh"));
